233f223d678b5c40578489e279bc0ccafcf79954
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-15 10:48:54 +0700

    start.prev = newNode (if start exists)

--- doubleLikedlist.cpp
+++ doubleLikedlist.cpp
@@ -44,6 +44,10 @@ class DoubleLinkedList
             }
             // Step 4: newNode.next = START
             newNode->next = START;
+
+            //Step 5: start.prev = newNode (if start exists)
+            if (START != NULL)
+            START->prev = newNode;
         }
 
     }
